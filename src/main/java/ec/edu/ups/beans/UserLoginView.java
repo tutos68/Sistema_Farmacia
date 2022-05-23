fcab91e782e5199b42186d2180e3ab4459ce4344
Adrian <luiscabreraucme@hotmail.com>
2022-05-22 22:56:20 -0500

    Correccion Dependencia

--- src/main/java/ec/edu/ups/beans/UserLoginView.java
+++ src/main/java/ec/edu/ups/beans/UserLoginView.java
@@ -1,13 +1,23 @@
 package ec.edu.ups.beans;
 
-
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.faces.application.FacesMessage;
 import jakarta.faces.context.FacesContext;
 import jakarta.inject.Named;
+import java.io.IOException;
+import java.net.Authenticator;
+import java.net.CookieHandler;
+import java.net.ProxySelector;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLParameters;
 import org.primefaces.PrimeFaces;
-
-
 
 /**
  *
@@ -16,41 +26,43 @@ import org.primefaces.PrimeFaces;
 @Named
 @RequestScoped
 public class UserLoginView {
-     
+
     private String username;
-     
+
     private String password;
- 
+
     public String getUsername() {
         return username;
     }
- 
+
     public void setUsername(String username) {
         this.username = username;
     }
- 
+
     public String getPassword() {
         return password;
     }
- 
+
     public void setPassword(String password) {
         this.password = password;
     }
-   
+
     public void login() {
         FacesMessage message = null;
         boolean loggedIn = false;
-         
-        if(username != null && username.equals("admin") && password != null && password.equals("admin")) {
+
+        if (username != null && username.equals("admin") && password != null && password.equals("admin")) {
             loggedIn = true;
             message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Bienvenido", username);
+            
             
         } else {
             loggedIn = false;
             message = new FacesMessage(FacesMessage.SEVERITY_WARN, "Loggin Error", "Credenciales Inválidas");
         }
-         
+
         FacesContext.getCurrentInstance().addMessage(null, message);
         PrimeFaces.current().ajax().addCallbackParam("loggedIn", loggedIn);
-    }   
+    }
+
 }
